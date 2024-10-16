33a92ee5f030ee9dc879e4d51d116fc9d1579fcc
Chanchal2318 <133849770+Chanchal2318@users.noreply.github.com>
2024-10-16 05:11:14 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+// alert(3+4);  // we are using node js no browser it can only possible in Browser . In nodejs , it gives us the error (alert is not defined)
+
+// code readability must be at high priority
+
+// Datatypes
+
+// number
+// bigint
+// string
+// boolean
+// null => standalone value
+// undefined => jab koi value define na ho
+// symbol => jab bhi baat krni ho unique
+
+
+// object
+console.log(typeof undefined)  // undefined is ans
+console.log(typeof null)  // object is an ans
