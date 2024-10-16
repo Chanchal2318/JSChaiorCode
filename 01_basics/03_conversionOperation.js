a150f98e681409abf27f7c33800990cafbc84ad9
Chanchal2318 <133849770+Chanchal2318@users.noreply.github.com>
2024-10-16 06:15:25 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "44"
+console.log(typeof score);
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+
+// After conversion
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+let isLoggenIn = 1
+let booleanIsLoggedIn = Boolean(isLoggenIn);
+console.log(booleanIsLoggedIn);
+
+
+// Conversion into a boolean type 
+//  "" -> false
+// "chanchal" -> true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
