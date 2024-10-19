1d556defa473bd5dda38ffdf97045c18788a72d1
Chanchal2318 <133849770+Chanchal2318@users.noreply.github.com>
2024-10-19 17:26:37 +0000

    ArrayPart2

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,31 +1,30 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
-// marvel_heros.push(dc_heros)
+ marvel_heros.push(dc_heros)
+ console.log(marvel_heros); // ["thor", "Ironman", "spiderman",["superman", "flash", "batman"]] This line ushes an array inside an another array.
+ console.log(marvel_heros[3][1]); //flash
 
-// console.log(marvel_heros);
-// console.log(marvel_heros[3][1]);
+ const allHeros = marvel_heros.concat(dc_heros)
+ console.log(allHeros); // // ["thor", "Ironman", "spiderman","superman", "flash", "batman"] It makes a new array and has all elements from both arrays.
 
-// const allHeros = marvel_heros.concat(dc_heros)
-// console.log(allHeros);
+const all_new_heros = [...marvel_heros, ...dc_heros] // spreading all the arrays.
 
-const all_new_heros = [...marvel_heros, ...dc_heros]
-
-// console.log(all_new_heros);
+console.log(all_new_heros); // ["thor", "Ironman", "spiderman","superman", "flash", "batman"]
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
-const real_another_array = another_array.flat(Infinity)
-console.log(real_another_array);
+const real_another_array = another_array.flat(Infinity) 
+console.log(real_another_array); //  [1, 2, 3, 4, 5, 6, 7, 6, 7, 4, 5]
 
 
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+console.log(Array.isArray("Hitesh")) // false
+console.log(Array.from("Hitesh")) // ['H','i','t','e','s','h']
+console.log(Array.from({name: "hitesh"})) // interesting and important // []
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+console.log(Array.of(score1, score2, score3)); // [100,200,300]
